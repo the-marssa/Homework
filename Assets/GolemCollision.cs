e948ad49c736986a36b85ddd5cd731c088b35347
Marssa <shevanat97@gmail.com>
2025-07-24 17:45:00 +0300

    Added trigger and collision interaction (OnTrigger & OnCollision)

---
+++ Assets/GolemCollision.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class GolemCollision : MonoBehaviour
+{
+    public GameObject messagePanel;
+    public TextMeshProUGUI messageText;
+
+    public string damageMessage = "Damage dealt to Golem: -1% HP";
+    public float displayTime = 2f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (messagePanel != null && messageText != null)
+            {
+                messagePanel.SetActive(true);
+                messageText.text = damageMessage;
+                Invoke(nameof(HideMessage), displayTime);
+            }
+        }
+    }
+
+    private void HideMessage()
+    {
+        if (messagePanel != null)
+            messagePanel.SetActive(false);
+    }
+}
